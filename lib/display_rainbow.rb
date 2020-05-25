39cd9c03a1c552f9fa54b1485d85c4f210c985e6
voracious-accelerator-8615 <margo.reiss.gold@gmail.com>
2020-05-25 02:48:05 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,7 +1,7 @@
 # Write your #display_rainbow method here
 colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
 def display_rainbow(colors)
-  puts "R: #{color[0]}, O: #{colors[1]}, Y: #{colors[2]}, Gcolors[3]}, B I V": #{
+  puts "R: #{color[0]}, O: #{colors[1]}, Y: #{colors[2]}, G: #{colors[3]}, B: #{colors[4]}, I: #{colors[5]}, V: #{colors[6]}""
   puts "R: #{colors[0]}"
   puts "O: #{colors[1]}"
   puts "Y: #{colors[2]}"
